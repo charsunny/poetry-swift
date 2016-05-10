00a4d6895962609e4371c29620974b9a01897197
sunxixi.sx <sunxixi.sx@alibaba-inc.com>
2016-05-10 19:53:36 +0800

    Initial Commit

---
+++ poetry/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  poetry
+//
+//  Created by 诺崇 on 16/5/10.
+//  Copyright © 2016年 诺崇. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
